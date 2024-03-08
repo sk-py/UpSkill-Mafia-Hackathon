f1116e3a144c6038dd54546119d162980360b957
sk-py <77550660+sk-py@users.noreply.github.com>
2024-03-08 23:57:55 +0530

    Initial Commit With Code

---
+++ athletex/src/app/page.js
@@ -0,0 +1,128 @@
+"use client";
+
+import { useState } from "react";
+
+import Link from "next/link";
+
+export default function Home() {
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <>
+      <section className="bg-white dark:bg-gray-900">
+        <nav className="container p-6 mx-auto lg:flex lg:justify-between lg:items-center">
+          <div className="flex items-center justify-between">
+            <span className="flex items-center justify-center h-max text-2xl text-white">
+              Athlete
+              <span className="font-bold text-blue-600 text-3xl ">X</span>
+            </span>
+            {/* Mobile menu button */}
+            <div className="flex lg:hidden">
+              <button
+                onClick={() => setIsOpen(!isOpen)}
+                type="button"
+                className="text-gray-500 dark:text-gray-200 hover:text-gray-600 dark:hover:text-gray-400 focus:outline-none focus:text-gray-600 dark:focus:text-gray-400"
+                aria-label="toggle menu"
+              >
+                {!isOpen ? (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="w-6 h-6"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M4 8h16M4 16h16"
+                    />
+                  </svg>
+                ) : (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="w-6 h-6"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M6 18L18 6M6 6l12 12"
+                    />
+                  </svg>
+                )}
+              </button>
+            </div>
+          </div>
+
+          {/* Mobile Menu open: "block", Menu closed: "hidden" */}
+          <div
+            className={`absolute inset-x-0 z-20 w-full px-6 py-4 transition-all duration-300 ease-in-out bg-white shadow-md lg:bg-transparent lg:dark:bg-transparent lg:shadow-none dark:bg-gray-900 lg:mt-0 lg:p-0 lg:top-0 lg:relative lg:w-auto lg:opacity-100 lg:translate-x-0 lg:flex lg:items-center ${
+              isOpen
+                ? "translate-x-0 opacity-100"
+                : "opacity-0 -translate-x-full"
+            }`}
+          >
+            <div className="flex flex-col space-y-4 lg:mt-0 lg:flex-row lg:-px-8 lg:space-y-0">
+              <Link
+                className="text-gray-700 transition-colors duration-300 transform lg:mx-8 dark:text-gray-200 dark:hover:text-blue-400 hover:text-blue-500"
+                href="#"
+              >
+                Home
+              </Link>
+              <Link
+                className="text-gray-700 transition-colors duration-300 transform lg:mx-8 dark:text-gray-200 dark:hover:text-blue-400 hover:text-blue-500"
+                href="#"
+              >
+                Events
+              </Link>
+              <Link
+                className="text-gray-700 transition-colors duration-300 transform lg:mx-8 dark:text-gray-200 dark:hover:text-blue-400 hover:text-blue-500"
+                href="#"
+              >
+                Buddies
+              </Link>
+            </div>
+
+            <Link
+              className="block px-5 py-2 mt-4 text-sm text-center text-white capitalize bg-blue-600 rounded-lg lg:mt-0 hover:bg-blue-500 lg:w-auto"
+              href={"/AuthPages"}
+            >
+              Sign In
+            </Link>
+          </div>
+        </nav>
+
+        <div className="container px-6 py-16 mx-auto text-center">
+          <div className="max-w-lg mx-auto">
+            <h1 className="text-3xl font-semibold text-gray-800 dark:text-white lg:text-4xl">
+              Connect with Fellow Sports Enthusiasts
+            </h1>
+            <p className="mt-6 text-gray-500 dark:text-gray-300">
+              Join our vibrant community to find like-minded individuals near
+              you, discover exciting events, and engage in your favorite sports
+              activities.
+            </p>
+            <button className="px-5 py-2 mt-6 text-sm font-medium leading-5 text-center text-white capitalize bg-blue-600 rounded-lg hover:bg-blue-500 lg:mx-0 lg:w-auto focus:outline-none">
+              Join Now
+            </button>
+            <p className="mt-3 text-sm text-gray-400 ">
+              No credit card required
+            </p>
+          </div>
+
+          <div className="flex justify-center mt-10">
+            <img
+              className="object-cover w-full h-96 rounded-xl lg:w-4/5"
+              src="/Hero.webp"
+              alt="App"
+            />
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
