12b66aeee07aeea4b0e6d05467b8ded2fd014819
sk-py <77550660+sk-py@users.noreply.github.com>
2024-03-10 00:14:57 +0530

    Login page added

--- athletex/src/app/login/page.js
+++ athletex/src/app/login/page.js
@@ -1,30 +1,29 @@
 "use client";
 
-export const Login = () => {
+const Login = () => {
   return (
     <section className="bg-white dark:bg-gray-900">
       <div className="flex justify-center min-h-screen">
         <div
           className="hidden bg-cover lg:block lg:w-2/5"
-          style="background-image: url('https://images.unsplash.com/photo-1494621930069-4fd4b2e24a11?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=715&q=80')"
-        ></div>
-
+          style={{
+            backgroundImage:
+              'url("https://images.unsplash.com/photo-1494621930069-4fd4b2e24a11?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=715&q=80")',
+          }}
+        />
         <div className="flex items-center w-full max-w-3xl p-8 mx-auto lg:px-12 lg:w-3/5">
           <div className="w-full">
             <h1 className="text-2xl font-semibold tracking-wider text-gray-800 capitalize dark:text-white">
               Get your free account now.
             </h1>
-
             <p className="mt-4 text-gray-500 dark:text-gray-400">
               Let’s get you all set up so you can verify your personal account
               and begin setting up your profile.
             </p>
-
             <div className="mt-6">
               <h1 className="text-gray-500 dark:text-gray-300">
                 Select type of account
               </h1>
-
               <div className="mt-3 md:flex md:items-center md:-mx-2">
                 <button className="flex justify-center w-full px-6 py-3 text-white bg-blue-500 rounded-lg md:w-auto md:mx-2 focus:outline-none">
                   <svg
@@ -33,18 +32,16 @@ export const Login = () => {
                     fill="none"
                     viewBox="0 0 24 24"
                     stroke="currentColor"
-                    stroke-width="2"
+                    strokeWidth={2}
                   >
                     <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
                       d="M21 13.255A23.931 23.931 0 0112 15c-3.183 0-6.22-.62-9-1.745M16 6V4a2 2 0 00-2-2h-4a2 2 0 00-2 2v2m4 6h.01M5 20h14a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
                     />
                   </svg>
-
                   <span className="mx-2">client</span>
                 </button>
-
                 <button className="flex justify-center w-full px-6 py-3 mt-4 text-blue-500 border border-blue-500 rounded-lg md:mt-0 md:w-auto md:mx-2 dark:border-blue-400 dark:text-blue-400 focus:outline-none">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
@@ -52,20 +49,18 @@ export const Login = () => {
                     fill="none"
                     viewBox="0 0 24 24"
                     stroke="currentColor"
-                    stroke-width="2"
+                    strokeWidth={2}
                   >
                     <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
                       d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"
                     />
                   </svg>
-
                   <span className="mx-2">worker</span>
                 </button>
               </div>
             </div>
-
             <form className="grid grid-cols-1 gap-6 mt-8 md:grid-cols-2">
               <div>
                 <label className="block mb-2 text-sm text-gray-600 dark:text-gray-200">
@@ -77,7 +72,6 @@ export const Login = () => {
                   className="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-lg dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40"
                 />
               </div>
-
               <div>
                 <label className="block mb-2 text-sm text-gray-600 dark:text-gray-200">
                   Last name
@@ -88,7 +82,6 @@ export const Login = () => {
                   className="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-lg dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40"
                 />
               </div>
-
               <div>
                 <label className="block mb-2 text-sm text-gray-600 dark:text-gray-200">
                   Phone number
@@ -99,7 +92,6 @@ export const Login = () => {
                   className="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-lg dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40"
                 />
               </div>
-
               <div>
                 <label className="block mb-2 text-sm text-gray-600 dark:text-gray-200">
                   Email address
@@ -110,7 +102,6 @@ export const Login = () => {
                   className="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-lg dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40"
                 />
               </div>
-
               <div>
                 <label className="block mb-2 text-sm text-gray-600 dark:text-gray-200">
                   Password
@@ -121,7 +112,6 @@ export const Login = () => {
                   className="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-lg dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40"
                 />
               </div>
-
               <div>
                 <label className="block mb-2 text-sm text-gray-600 dark:text-gray-200">
                   Confirm password
@@ -132,10 +122,8 @@ export const Login = () => {
                   className="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-lg dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40"
                 />
               </div>
-
               <button className="flex items-center justify-between w-full px-6 py-3 text-sm tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue-500 rounded-lg hover:bg-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-50">
                 <span>Sign Up </span>
-
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   className="w-5 h-5 rtl:-scale-x-100"
@@ -143,9 +131,9 @@ export const Login = () => {
                   fill="currentColor"
                 >
                   <path
-                    fill-rule="evenodd"
+                    fillRule="evenodd"
                     d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
-                    clip-rule="evenodd"
+                    clipRule="evenodd"
                   />
                 </svg>
               </button>
@@ -156,3 +144,5 @@ export const Login = () => {
     </section>
   );
 };
+
+export default Login;
--- athletex/src/app/page.js
+++ athletex/src/app/page.js
@@ -3,7 +3,7 @@
 import { useState } from "react";
 
 import Link from "next/link";
-import FAQ from "../components/FAQ.jsx";
+import FAQ from "./components/FAQ.jsx";
 
 export default function Home() {
   const [isOpen, setIsOpen] = useState(false);
@@ -92,7 +92,7 @@ export default function Home() {
 
             <Link
               className="block px-5 py-2 mt-4 text-sm text-center text-white capitalize bg-blue-600 rounded-lg lg:mt-0 hover:bg-blue-500 lg:w-auto"
-              href={"/app/login"}
+              href={"/login"}
             >
               Sign In
             </Link>
